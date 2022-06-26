530a6c5c7b76061c904ea4b27a33399a01933966
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-26 22:13:00 +0800

    Update CORS

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const cors  = require('cors')
 const { auth } = require('./api')
 
 module.exports = async (app) => {
-    app.use(cors())
+    app.use(cors({
+        credentials: true,
+        origin: 'http://localhost:3000'
+    }))
 
     auth(app)
 }
